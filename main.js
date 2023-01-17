676123c54d1a59fcd1f275bee35cc7a800f90eda
MKarpovski <113193440+MKarpovski@users.noreply.github.com>
2023-01-16 19:52:23 -0500

    1/16

--- main.js
+++ main.js
@@ -72,6 +72,86 @@ const products = [
      {name:Razer_Keyboard ,
       image: ,
       price:79.99 ,
+      category: Keyboards,Computers 
+    }
+     {name:Dyson_Pure_Hot_Cool,
+      image: //dyson-h.assetsadobe2.com/is/image/content/dam/dyson/images/products/primary/311383-01.png?$responsive$&cropPathE=tablet&fit=stretch,1&wid=1024
+      price: 399.99,
+      category:Home,Decor , 
+    }
+     {name: LG_refrigirator,
+      image: ,
+      price:.99 ,
+      category:Home ,Kitchen}
+
+     {name:cuisinart_blender,
+      image: ,
+      price:99.99 ,
+      category:kitchen, Home
+    }
+     {name:DJI_drone,
+      image: ,
+      price:759.99 ,
+      category:camera, drones , 
+    }
+     {name:Segway_Scoter
+      image: ,
+      price: 779.99,
+      category: , 
+    }
+     {name:Fitbit_sense,
+      image: ,
+      price: 189.99,
+      category: , 
+    }
+     {name:Star_wars_toy,
+      image: ,
+      price:22.49 ,
+      category:toys,entertainment , 
+    }
+     {name:Netgear_router,
+      image: ,
+      price:299.99 ,
+      category:Home , 
+    }
+     {name:Nest_hub,
+      image: ,
+      price:189.99 ,
+      category:ho , 
+    }
+     {name:Google_Nest_camera,
+      image: ,
+      price:269.99 ,
+      category:home , 
+    }
+     {name:Amazon_Echo,
+      image: ,
+      price:44.99 ,
+      category:home , 
+    }
+     {name:,
+      image: ,
+      price: ,
+      category: , 
+    }
+     {name:,
+      image: ,
+      price: ,
+      category: , 
+    }
+     {name:,
+      image: ,
+      price: ,
+      category: , 
+    }
+     {name:,
+      image: ,
+      price: ,
+      category: , 
+    }
+     {name:,
+      image: ,
+      price: ,
       category: , 
     }
     
